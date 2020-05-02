28090066194fb3ed5f3a1eddd6d806d63b56aae3
mohannautiyal <mohannautiyal80@gmail.com>
2020-05-02 23:32:43 +0530

    Misc Java Prog

---
+++ MiscJavaProg/src/com/home/fibonacciSeries.java
@@ -0,0 +1,27 @@
+package com.home;
+
+public class fibonacciSeries {
+	
+	public static void main(String[] args) {
+		
+		
+		int n =20;
+		int n1 =0;
+		int n2=1;
+		int temp=0;
+
+		while(n>0) {
+			System.out.print(n1+"  ");
+
+			temp = n1+n2;
+			n1 =n2;
+			n2 =temp;
+     		n--;
+			
+		}
+		
+	}
+	
+	
+
+}
