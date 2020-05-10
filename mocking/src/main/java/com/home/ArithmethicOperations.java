fbad55f77e3db58d07d1ee96c51063637101f930
mohannautiyal <mohannautiyal80@gmail.com>
2020-05-10 20:26:53 +0530

    Mocking

---
+++ mocking/src/main/java/com/home/ArithmethicOperations.java
@@ -0,0 +1,24 @@
+package com.home;
+
+public class ArithmethicOperations {
+
+	
+	public int add(int a, int b) {
+		int sum = 0;
+		System.out.println("Sum of numbers is not implemented yet " + sum);
+		return 0;
+	}
+	
+	
+	public int substract(int a, int b) {
+		int diff = a-b;
+		System.out.println("Diff of numbers is "+ diff);
+		return diff;
+	}
+	
+	
+	
+	
+	
+	
+}
---
+++ mocking/src/test/java/com/home/TestCalculate.java
@@ -0,0 +1,38 @@
+package com.home;
+
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.when;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+public class TestCalculate {
+
+	
+	@Test
+	public void TestAdd() {
+		ArithmethicOperations Operations = new ArithmethicOperations();
+		int sum =Operations.add(30, 45);
+		assertTrue("Sum of numbers is incorrect", sum==75);
+		
+	}
+	
+	// Below test uses mocking
+	@Test
+	public void TestMockAdd() {
+		
+		ArithmethicOperations Operations = Mockito.mock(ArithmethicOperations.class);
+		when(Operations.add(30,45)).thenReturn(75);
+		int sum =Operations.add(30, 45);
+		System.out.println("Mocking sum operations "+sum);
+		assertTrue("Sum of numbers is incorrect", sum==75);
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
